d2e84ef9f908c6c6b46b6da7b427d2f194c2bb76
ytx1991 <ytx1991@gmail.com>
2021-06-26 18:39:00 -0700

    Add reward distribute script

---
+++ reward.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Your total shares
+totalShare=100
+# Your wallet password
+export HELIUM_WALLET_PASSWORD=YOU_PASSWORD
+# Payee address
+address=(1bxzZ9SdeToPsyXUCt7g6uENE7bpzAnTWJqhutUgPd5ARHiYyyu)
+# Payee name
+name=("test1")
+# Payee shares
+share=(10)
+# Reserved HNT for operation purpose
+reserveAmount=50
+total=${#name[*]}
+#Truncated balance in the wallet
+balance=$(./helium-wallet -f wallet.key.1 balance | grep -P '[\d]+\.[\d]+' -o | head -n 1 | grep -o '^[0-9]*')
+echo "$(date -u)        Current balance $balance"
+rewards=$((balance - reserveAmount))
+signature=$(openssl rand -base64 8)
+echo "$(date -u)        Reserved: $reserveAmount HNT, distribute: $rewards HNT, sig: $signature"
+for (( i=0; i<=$(( $total -1 )); i++ ))
+do
+        pay=$((rewards / totalShare * ${share[$i]}))
+        echo "$(date -u)        Paying ${name[$i]} addr: ${address[$i]} amount: $pay ..."
+        ./helium-wallet -f wallet.key.1 -f wallet.key.2 -f wallet.key.3 pay one ${address[$i]} $pay --memo $signature --commit
+done

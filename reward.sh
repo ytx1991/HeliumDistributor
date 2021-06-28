d3c462f09199a3b7f9a167c383c0124b9f50c821
ytx1991 <ytx1991@gmail.com>
2021-06-27 17:01:56 -0700

    Skip payment when there is any unpaid

--- reward.sh
+++ reward.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#Check if there is anyone unpaid
+unpaid=$(ls -l | grep unpaid | wc -l)
+if [[ $unpaid -gt 0 ]]
+then
+        echo "Found $unpaid unpaid, skipping the current payment ..."
+        exit
+fi
 # Your total shares
 totalShare=100
 # Your wallet password
@@ -9,6 +16,8 @@ address=(1aVMj6QWrRAuF4DwKqpzP7ShXMoKGWnEiqvf1wZwjjZijeiUD8g)
 name=("test1")
 # Payee shares
 share=(10)
+# Unpaid payee
+unpaid=
 # Reserved HNT for operation purpose
 reserveAmount=50
 total=${#name[*]}
@@ -23,5 +32,4 @@ do
         pay=$((rewards / totalShare * ${share[$i]}))
         echo "$(date -u)        Paying ${name[$i]}, addr: ${address[$i]}, amount: $pay, signature: $signature ..."
         ./helium-wallet -f wallet.key.1 -f wallet.key.2 -f wallet.key.3 pay one ${address[$i]} $pay --memo $signature --commit || echo $pay > ${name[$i]}-$signature.unpaid
-        echo "$(date -u)        Paid ${name[$i]}"
 done

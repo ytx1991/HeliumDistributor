d90bb880f89e2a00703295a8dfe5384e455e1fb3
ytx1991 <ytx1991@gmail.com>
2021-06-28 18:36:22 -0700

    Save log for each batch

--- reward.sh
+++ reward.sh
@@ -10,26 +10,26 @@ fi
 totalShare=100
 # Your wallet password
 export HELIUM_WALLET_PASSWORD=
+#export HELIUM_API_URL=https://testnet-api.helium.wtf/v1
 # Payee address
 address=(1aVMj6QWrRAuF4DwKqpzP7ShXMoKGWnEiqvf1wZwjjZijeiUD8g)
 # Payee name
 name=("test1")
 # Payee shares
 share=(10)
-# Unpaid payee
-unpaid=
 # Reserved HNT for operation purpose
 reserveAmount=50
 total=${#name[*]}
+signature=$(openssl rand -base64 8)
+mkdir $signature
 #Truncated balance in the wallet
 balance=$(./helium-wallet -f wallet.key.1 balance | grep -P '[\d]+\.[\d]+' -o | head -n 1 | grep -o '^[0-9]*')
-echo "$(date -u)        Current balance $balance"
+echo "$(date -u)        Current balance $balance" >> $signature/log
 rewards=$((balance - reserveAmount))
-signature=$(openssl rand -base64 8)
-echo "$(date -u)        Reserved: $reserveAmount HNT, distribute: $rewards HNT, sig: $signature"
+echo "$(date -u)        Reserved: $reserveAmount HNT, distribute: $rewards HNT, sig: $signature" >> $signature/log
 for (( i=0; i<=$(( $total -1 )); i++ ))
 do
         pay=$((rewards / totalShare * ${share[$i]}))
-        echo "$(date -u)        Paying ${name[$i]}, addr: ${address[$i]}, amount: $pay, signature: $signature ..."
-        ./helium-wallet -f wallet.key.1 -f wallet.key.2 -f wallet.key.3 pay one ${address[$i]} $pay --memo $signature --commit || echo $pay > ${name[$i]}-$signature.unpaid
+        echo "$(date -u)        Paying ${name[$i]}, addr: ${address[$i]}, amount: $pay, signature: $signature ..." >> $signature/log
+        ./helium-wallet -f wallet.key.1 -f wallet.key.2 -f wallet.key.3 pay one ${address[$i]} $pay --memo $signature --commit >> $signature/log || echo $pay > $signature/${name[$i]}-$signature.unpaid
 done
